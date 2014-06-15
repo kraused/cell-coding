3877f01e37cf76d3effde1d4cae5cab308c07678
Dorian Krause <kraused@91df63fb.com>
2014-06-15 14:15:32 +0200

    Add ppu2 benchmark
    This is a version of ppu1
    with manual vectorization.

---
+++ stream/ppu2/main.c
@@ -0,0 +1,113 @@
+
+#include "common/types.h"
+
+#include <altivec.h>
+#include <stdio.h>
+
+#define FLT 	float
+#define LEN 	(32*1024*1024/sizeof(float))
+#define NTIMES 	10
+
+static inline void stream_copy(int N, float *restrict a, float *restrict c)
+{
+	int i;
+
+	for (i = 0; (i + 4) < N; i += 4) {
+		vec_st(vec_ld(i * sizeof(float), &a[0]), 
+		       i * sizeof(float), &c[0]);
+	}
+
+	for(; i < N; ++i) {
+		c[i] = a[i];
+	}
+}
+
+static inline void stream_scale(int N, float *restrict b, float *restrict c, 
+                                float scalar)
+{
+	int i;
+	vector float v;
+	vector float s = (vector float){ 
+		scalar, scalar, scalar, scalar
+	};
+	const vector float zero = (vector float){ 0, 0, 0, 0 };
+
+	for (i = 0; (i + 4) < N; i += 4) {
+		v = vec_ld(i * sizeof(float), &c[0]);
+		vec_st(vec_madd(s, v, zero), 
+		       i * sizeof(float), &b[0]);
+	}
+
+	for (; i < N; ++i) {
+		b[i] = scalar*c[i];
+	}
+}
+
+static inline void stream_add(int N, float *restrict a, float *restrict b, 
+                              float *restrict c)
+{
+	int i;
+	vector float v, w;
+
+	for (i = 0; (i + 4) < N; i += 4) {
+		v = vec_ld(i * sizeof(float), &a[0]);
+		w = vec_ld(i * sizeof(float), &b[0]);
+		vec_st(vec_add(v, w), 
+		       i * sizeof(float), &c[0]);
+	}
+
+	for (; i < N; ++i) {
+		c[i] = a[i] + b[i];
+	}
+}
+
+static inline void stream_triad(int N, float *restrict a, float *restrict b, 
+                                float *restrict c, float scalar)
+{
+	int i;
+	vector float v, w;
+	vector float s = (vector float){ 
+		scalar, scalar, scalar, scalar
+	};
+
+	for (i = 0; (i + 4) < N; i += 4) {
+		v = vec_ld(i * sizeof(float), &b[0]);
+		w = vec_ld(i * sizeof(float), &c[0]);
+		vec_st(vec_madd(s, w, v), 
+		       i * sizeof(float), &a[0]);
+	}
+
+	for (; i < N; ++i) {
+		a[i] = b[i] + scalar*c[i];
+	}
+}
+
+#include "stream/stream.h"
+
+int main(int argc, char **argv)
+{
+	double times[NTIMES-1][4];
+	float *restrict a;
+	float *restrict b;
+	float *restrict c;
+	const float scalar = 3;
+	int retval;
+
+	retval = stream_alloc_arrays_on_ppu(LEN, 128, &a, &b, &c);
+	if (unlikely(retval)) {
+		fprintf(stderr, "stream_alloc_arrays_on_ppu failed with error %d.\n", retval);
+		return 1;
+	}
+
+	stream_fill_arrays_on_ppu(LEN, a, b, c);
+	
+	printf(" Starting kernel.\n");
+	stream_execute_main_loop_on_ppu(LEN, NTIMES, a, b, c, scalar, times);
+	
+	stream_print_results_on_ppu(LEN, NTIMES, times);
+	stream_validate_arrays_on_ppu(LEN, NTIMES, 1e-5, a, b, c, scalar);
+
+	stream_free_arrays_on_ppu(&a, &b, &c);
+
+	return 0;
+}
