1f5fa6e5b50581c919e582a8a1b4aa3ee6a2b8ee
Dorian Krause <kraused@91df63fb.com>
2014-06-15 20:25:12 +0200

    Add spu2 benchmark
    This commit adds a version
    of the SPU benchmark that
    avoids the overhead of thread
    creation and tear down by
    keeping the threads alive
    and using mailboxes for messaging.

---
+++ stream/spu2/args.h
@@ -0,0 +1,14 @@
+
+#ifndef CELL_CODING_STREAM_SPU1_ARGS_H
+#define CELL_CODING_STREAM_SPU1_ARGS_H 1
+
+struct spu1_kernel_args {
+	int		N;
+	float *restrict	a;
+	float *restrict	b;
+	float *restrict	c;
+	float		scalar;
+} __attribute__((aligned(128)));
+
+#endif
+
