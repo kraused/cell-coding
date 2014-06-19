ddea2dd938af1055ac6e891bb31620e1a0b85328
Dorian Krause <kraused@91df63fb.com>
2014-06-19 19:15:57 +0200

    Add spu3 benchmark
    This commit adds a new
    version of the SPU benchmark.
    The difference to spu2
    is a reduced number of calls
    to mfc_read_tag_status_all().

---
+++ stream/spu3/main.c
@@ -0,0 +1,273 @@
+
+#include "common/types.h"
+#include "common/compiler.h"
+
+#include "stream/spu2/args.h"
+#include "stream/spu2/msg.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <libspe2.h>
+#include <pthread.h>
+
+#define FLT 	float
+/*
+ * It is best if LEN is divisible by the number
+ * of SPUs.
+ */
+#define LEN 	(6*128*64*1024/sizeof(FLT))
+#define NTIMES 	10
+
+struct ppu_pthread_data {
+	spe_context_ptr_t	id;
+	pthread_t 		pthread;
+	void 			*argp;
+};
+
+void *ppu_pthread_function(void *arg)
+{
+	struct ppu_pthread_data *data = (struct ppu_pthread_data *)arg;
+	int retval;
+	unsigned int entry = SPE_DEFAULT_ENTRY;
+
+	retval = spe_context_run(data->id, &entry, 0,
+	                         data->argp, NULL, NULL);
+	if (unlikely(retval < 0)) {
+		perror("spe_context_run");
+		exit(128);
+	}
+
+	pthread_exit(NULL);
+}
+
+#ifndef MAX_SPUS
+# define MAX_SPUS 16
+#endif
+struct ppu_pthread_data data[MAX_SPUS];
+struct spu1_kernel_args args[MAX_SPUS];
+
+static int num_usable_spus()
+{
+	int nspus;
+	char *tailptr;
+
+	if (getenv("NSPUS")) {
+		nspus = (int )strtol(getenv("NSPUS"), &tailptr, 0);
+	} else {
+		nspus = spe_cpu_info_get(SPE_COUNT_USABLE_SPES, 0);
+	}
+
+	return nspus;
+}
+
+static int exec_spu_program(spe_program_handle_t *handle, int N,
+                            float *restrict a, float *restrict b,
+                            float *restrict c, float scalar)
+{
+	int i, nspus;
+	int retval;
+
+	nspus = num_usable_spus();
+
+	for (i = 0; i < nspus; ++i) {
+		args[i].N      = N / nspus;
+		args[i].a      = a + i * args[i].N;
+		args[i].b      = b + i * args[i].N;
+		args[i].c      = c + i * args[i].N;
+		args[i].scalar = scalar;
+	}
+
+	/* The last spu has to do more work if N is not divisible by nspus.
+	 */	
+	args[nspus-1].N = N - (nspus - 1)*((int )(N / nspus));
+
+	for (i = 0; i < nspus; ++i) {
+		if (NULL == (data[i].id = spe_context_create(0, NULL))) {
+			perror("spe_context_create");
+			return retval;
+		}
+
+		retval = spe_program_load(data[i].id, handle);
+		if (unlikely(retval)) {
+			perror("spe_program_load");
+			return retval;
+		}
+
+		data[i].argp = &args[i];
+
+		retval = pthread_create(&data[i].pthread, NULL,
+		                        ppu_pthread_function, &data[i]);
+		if (unlikely(retval)) {
+			perror("pthread_create");
+			return retval;
+		}
+	}
+
+	return 0;
+}
+
+/*
+ * Stop the execution of all SPU threads.
+ */
+static int stop_spus()
+{
+	int i, nspus;
+	int retval;
+	unsigned int msg = SPU2_MSG_PPU_TO_SPU_EXIT;
+
+	nspus = num_usable_spus();
+
+	for (i = 0; i < nspus; ++i) {
+		retval = spe_in_mbox_write(data[i].id, &msg, 1,
+		                           SPE_MBOX_ALL_BLOCKING);
+		if (unlikely(1 != retval)) {
+			perror("spe_in_mbox_write");
+			return retval;
+		}
+
+		retval = pthread_join(data[i].pthread, NULL);
+		if (unlikely(retval)) {
+			perror("pthread_join");
+			return retval;
+		}
+
+		retval = spe_context_destroy(data[i].id);
+		if (unlikely(retval)) {
+			perror("spe_context_destroy");
+			return retval;
+		}
+	}
+
+	return 0;
+}
+
+/*
+ * Broadcast msg to all SPUs via the mailbox. 
+ */
+static int bcast_to_spus(unsigned int msg)
+{
+	int i, nspus;
+	int retval;
+
+	nspus = num_usable_spus();
+
+	for (i = 0; i < nspus; ++i) {
+		retval = spe_in_mbox_write(data[i].id, &msg, 1,
+		                           SPE_MBOX_ALL_BLOCKING);
+		if (unlikely(1 != retval)) {
+			perror("spe_in_mbox_write");
+			return retval;
+		}
+	}
+
+	return 0;
+}
+
+/*
+ * Wait for all SPUs to send the DONE signal.
+ */
+static int wait_for_spus_done()
+{
+	int i, nspus, done;
+	int retval;
+	unsigned int stats[MAX_SPUS];
+
+	nspus = num_usable_spus();
+
+	memset(stats, 0, sizeof(stats));
+	done = 0;
+
+	while (nspus != done) {
+		for (i = 0; i < nspus; ++i) {
+			if(stats[i])
+				continue;
+
+			retval = spe_out_mbox_status(data[i].id);
+			if (retval > 0) {
+				retval = spe_out_mbox_read(data[i].id, &stats[i], 1);
+				if (unlikely(1 != retval)) {
+					perror("spe_out_mbox_read");
+					return -1;
+				}
+				if (unlikely(SPU2_MSG_SPU_TO_PPU_DONE != stats[i])) {
+					fprintf(stderr, " dropping unexpected message"
+					                " in wait_for_spus_done.\n");
+					stats[i] = 0;
+				}
+				
+				++done;				
+			}
+		}
+	}
+
+	return 0;
+}
+
+static inline void stream_copy(int N, float *restrict a, float *restrict c)
+{
+	bcast_to_spus(SPU2_MSG_PPU_TO_SPU_DO_COPY);
+	wait_for_spus_done();
+}
+
+static inline void stream_scale(int N, float *restrict b, float *restrict c, 
+                                float scalar)
+{
+	bcast_to_spus(SPU2_MSG_PPU_TO_SPU_DO_SCALE);
+	wait_for_spus_done();
+}
+
+static inline void stream_add(int N, float *restrict a, float *restrict b, 
+                              float *restrict c)
+{
+	bcast_to_spus(SPU2_MSG_PPU_TO_SPU_DO_ADD);
+	wait_for_spus_done();
+}
+
+static inline void stream_triad(int N, float *restrict a, float *restrict b, 
+                                float *restrict c, float scalar)
+{
+	bcast_to_spus(SPU2_MSG_PPU_TO_SPU_DO_TRIAD);
+	wait_for_spus_done();
+}
+
+#include "stream/stream.h"
+
+extern spe_program_handle_t stream_spu;
+
+int main(int argc, char **argv)
+{
+	double times[NTIMES-1][4];
+	float *restrict a;
+	float *restrict b;
+	float *restrict c;
+	const float scalar = 3;
+	int retval;
+
+	retval = stream_alloc_arrays_on_ppu(LEN, 128, &a, &b, &c);
+	if (unlikely(retval)) {
+		fprintf(stderr, " stream_alloc_arrays_on_ppu failed with error %d.\n", retval);
+		return 1;
+	}
+
+	stream_fill_arrays_on_ppu(LEN, a, b, c);
+
+	retval = exec_spu_program(&stream_spu, LEN, a, b, c, scalar);
+	if (unlikely(retval)) {
+		fprintf( stderr, " exec_spu_program failed with error %d.\n", retval);
+		return 1;
+	}
+	
+	printf(" Starting kernel.\n");
+	stream_execute_main_loop_on_ppu(LEN, NTIMES, a, b, c, scalar, times);
+	
+	stream_print_results_on_ppu(LEN, NTIMES, times);
+	stream_validate_arrays_on_ppu(LEN, NTIMES, 1e-5, a, b, c, scalar);
+
+	stop_spus();
+
+	stream_free_arrays_on_ppu(&a, &b, &c);
+
+	return 0;
+}
---
+++ stream/spu3/spu.c
@@ -0,0 +1,259 @@
+
+#include "common/types.h"
+#include "common/compiler.h"
+
+#include "stream/spu1/args.h"
+#include "stream/spu2/msg.h"
+
+#include <spu_intrinsics.h>
+#include <spu_mfcio.h>
+
+#include <stdio.h>
+
+/* Should be a multiple of 128 / sizeof(float) = 32 */
+#define SIZE	256
+#define TAG	3
+
+volatile struct spu1_kernel_args args __attribute__((aligned(128)));
+
+volatile vector float ls1[SIZE / 4] __attribute__((aligned(128)));
+volatile vector float ls2[SIZE / 4] __attribute__((aligned(128)));
+volatile vector float ls3[SIZE / 4] __attribute__((aligned(128)));
+
+void copy()
+{
+	int i, j, n;
+
+	n = SIZE * sizeof(float);
+
+	for (i = 0; (i + SIZE) < args.N; i += SIZE) {
+		mfc_get((void *)&ls1[0], (unsigned int )&args.a[i], n, TAG, 0, 0);
+		mfc_write_tag_mask(1 << TAG);
+		mfc_read_tag_status_all();
+		
+		for (j = 0; j < (SIZE / 4); ++j)
+			ls2[j] = ls1[j];
+
+		mfc_put((void *)&ls2[0], (unsigned int )&args.c[i], n, TAG, 0, 0);
+	}
+
+	mfc_write_tag_mask(1 << TAG);
+	mfc_read_tag_status_all();
+
+	if (unlikely(i < args.N)) {
+		/* 
+		 * args.N - i will be smaller than SIZE at this point so
+		 * it is safe to do a DMA transfer.
+		 * We need to make sure that size is a multiple of 16.
+		 */
+		n = ((args.N - i) * sizeof(float)) & (~127);
+
+		mfc_get((void *)&ls1[0], (unsigned int )&args.a[i], n, TAG, 0, 0);
+		mfc_write_tag_mask(1 << TAG);
+		mfc_read_tag_status_all();
+		
+		/* n must be divisible by 4. */
+		for (j = 0; j < ((args.N - i) / 4); ++j)
+			ls2[j] = ls1[j];
+		
+		mfc_put((void *)&ls2[0], (unsigned int )&args.c[i], n, TAG, 0, 0);
+	}
+
+	mfc_write_tag_mask(1 << TAG);
+	mfc_read_tag_status_all();
+
+	/* 
+	 * At this point it may be that i is still smaller than args.N if the length
+	 * was not divisible by the number of SPUs times 16.
+	 */
+}
+
+void scale()
+{
+	int i, j, n;
+
+	vector float s = spu_splats(args.scalar);
+	vector float z = spu_splats(0.0f);
+
+	n = SIZE * sizeof(float);
+
+	for (i = 0; (i + SIZE) < args.N; i += SIZE) {
+		mfc_get((void *)&ls1[0], (unsigned int )&args.c[i], n, TAG, 0, 0);
+		mfc_write_tag_mask(1 << TAG);
+		mfc_read_tag_status_all();
+
+		for (j = 0; j < (SIZE / 4); ++j)
+			ls2[j] = spu_madd(s, ls1[j], z);
+
+		mfc_put((void *)&ls2[0], (unsigned int )&args.b[i], n, TAG, 0, 0);
+	}
+		
+	mfc_write_tag_mask(1 << TAG);
+	mfc_read_tag_status_all();
+
+	if (unlikely(i < args.N)) {
+		/* 
+		 * args.N - i will be smaller than SIZE at this point so
+		 * it is safe to do a DMA transfer.
+		 * We need to make sure that size is a multiple of 16.
+		 */
+		n = ((args.N - i) * sizeof(float)) & (~127);
+
+		mfc_get((void *)&ls1[0], (unsigned int )&args.c[i], n, TAG, 0, 0);
+		mfc_write_tag_mask(1 << TAG);
+		mfc_read_tag_status_all();
+		
+		/* n must be divisible by 4. */
+		for (j = 0; j < ((args.N - i) / 4); ++j)
+			ls2[j] = spu_madd(s, ls1[j], z);
+		
+		mfc_put((void *)&ls2[0], (unsigned int )&args.b[i], n, TAG, 0, 0);
+	}
+		
+	mfc_write_tag_mask(1 << TAG);
+	mfc_read_tag_status_all();
+
+	/* 
+	 * At this point it may be that i is still smaller than args.N if the length
+	 * was not divisible by the number of SPUs times 16.
+	 */
+}
+
+void add()
+{
+	int i, j, n;
+	
+	n = SIZE * sizeof(float);
+
+	for (i = 0; (i + SIZE) < args.N; i += SIZE) {
+		mfc_get((void *)&ls1[0], (unsigned int )&args.a[i], n, TAG, 0, 0);
+		mfc_get((void *)&ls2[0], (unsigned int )&args.b[i], n, TAG, 0, 0);
+		mfc_write_tag_mask(1 << TAG);
+		mfc_read_tag_status_all();
+
+		for (j = 0; j < (SIZE / 4); ++j)
+			ls3[j] = spu_add(ls1[j], ls2[j]);
+
+		mfc_put((void *)&ls3[0], (unsigned int )&args.c[i], n, TAG, 0, 0);
+	}
+		
+	mfc_write_tag_mask(1 << TAG);
+	mfc_read_tag_status_all();
+
+	if (unlikely(i < args.N)) {
+		/* 
+		 * args.N - i will be smaller than SIZE at this point so
+		 * it is safe to do a DMA transfer.
+		 * We need to make sure that size is a multiple of 16.
+		 */
+		n = ((args.N - i) * sizeof(float)) & (~127);
+
+		mfc_get((void *)&ls1[0], (unsigned int )&args.a[i], n, TAG, 0, 0);
+		mfc_get((void *)&ls2[0], (unsigned int )&args.b[i], n, TAG, 0, 0);
+		mfc_write_tag_mask(1 << TAG);
+		mfc_read_tag_status_all();
+		
+		/* n must be divisible by 4. */
+		for (j = 0; j < ((args.N - i) / 4); ++j)
+			ls3[j] = spu_add(ls1[j], ls2[j]);
+		
+		mfc_put((void *)&ls3[0], (unsigned int )&args.c[i], n, TAG, 0, 0);
+	}
+		
+	mfc_write_tag_mask(1 << TAG);
+	mfc_read_tag_status_all();
+
+	/* 
+	 * At this point it may be that i is still smaller than args.N if the length
+	 * was not divisible by the number of SPUs times 16.
+	 */
+}
+
+void triad()
+{
+	int i, j, n;
+	
+	vector float s = spu_splats(args.scalar);
+	
+	n = SIZE * sizeof(float);
+
+	for (i = 0; (i + SIZE) < args.N; i += SIZE) {
+		mfc_get((void *)&ls1[0], (unsigned int )&args.b[i], n, TAG, 0, 0);
+		mfc_get((void *)&ls2[0], (unsigned int )&args.c[i], n, TAG, 0, 0);
+		mfc_write_tag_mask(1 << TAG);
+		mfc_read_tag_status_all();
+
+		for (j = 0; j < (SIZE / 4); ++j)
+			ls3[j] = spu_madd(s, ls2[j], ls1[j]);
+
+		mfc_put((void *)&ls3[0], (unsigned int )&args.a[i], n, TAG, 0, 0);
+	}
+		
+	mfc_write_tag_mask(1 << TAG);
+	mfc_read_tag_status_all();
+
+	if (unlikely(i < args.N)) {
+		/* 
+		 * args.N - i will be smaller than SIZE at this point so
+		 * it is safe to do a DMA transfer.
+		 * We need to make sure that size is a multiple of 16.
+		 */
+		n = ((args.N - i) * sizeof(float)) & (~127);
+
+		mfc_get((void *)&ls1[0], (unsigned int )&args.b[i], n, TAG, 0, 0);
+		mfc_get((void *)&ls2[0], (unsigned int )&args.c[i], n, TAG, 0, 0);
+		mfc_write_tag_mask(1 << TAG);
+		mfc_read_tag_status_all();
+		
+		/* n must be divisible by 4. */
+		for (j = 0; j < ((args.N - i) / 4); ++j)
+			ls3[j] = spu_madd(s, ls2[j], ls1[j]);
+		
+		mfc_put((void *)&ls3[0], (unsigned int )&args.a[i], n, TAG, 0, 0);
+	}
+		
+	mfc_write_tag_mask(1 << TAG);
+	mfc_read_tag_status_all();
+
+	/* 
+	 * At this point it may be that i is still smaller than args.N if the length
+	 * was not divisible by the number of SPUs times 16.
+	 */
+}
+
+int main(ull id, ull argp, ull envp)
+{
+	unsigned int cmd;
+
+	mfc_get(&args, argp, sizeof(args), TAG, 0, 0);
+	mfc_write_tag_mask(1 << TAG);
+	mfc_read_tag_status_all();
+
+	while (1) {
+		cmd = spu_read_in_mbox();
+
+		if (unlikely(SPU2_MSG_PPU_TO_SPU_EXIT == cmd))
+			break;
+
+		switch (cmd) {
+		case SPU2_MSG_PPU_TO_SPU_DO_COPY:
+			copy();
+			break;
+		case SPU2_MSG_PPU_TO_SPU_DO_SCALE:
+			scale();
+			break;
+		case SPU2_MSG_PPU_TO_SPU_DO_ADD:
+			add();
+			break;
+		case SPU2_MSG_PPU_TO_SPU_DO_TRIAD:
+			triad();
+			break;
+		default:
+			fprintf(stderr, " [SPU]: Invalid command received in mailbox\n");
+		}
+
+		spu_write_out_mbox(SPU2_MSG_SPU_TO_PPU_DONE);
+	}
+
+	return 0;
+}
